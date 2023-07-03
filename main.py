ece516ebbc166a4b01ef9565ce0497570b8f596f
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-07-03 19:48:57 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import re
+# strTxt = input("enter txt -> ")
+strTxt = "-12.3-35=40, 50*10+30/40 Hello, " \
+         ":!.word2023!0.23/cup:?- How\nis\tgoing your deals?" \
+         " Are you ready.for New Summer 2024."
