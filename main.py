99f8da2a845c2f7ab4e82791d58d279cdd656ccf
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-07-04 23:44:58 +0300

    4 Done logic of all functions in sub menu "2. Reports"

--- main.py
+++ main.py
@@ -1,62 +1,62 @@
-import re
-# strTxt = input("enter txt -> ")
-txt = "-12.3-35=40, 50*10+30/40 Hello, " \
+
+def arr_word_num(txt):
+    print(txt)
+    symbols2 = "!?.,:;+-*/"
+    resAlpha: str = ''
+    resNumer: str = ''
+    resTxt = []
+    for i in range(len(txt)):
+        if txt[i].isalpha():
+            resAlpha += txt[i]
+        else:
+            if resAlpha != '':
+                resTxt.append(resAlpha)
+                resAlpha = ''
+            else:
+                continue
+    resTxt.append(resAlpha)
+
+    for i in range(len(txt)):
+        if txt[i].isnumeric():
+            resNumer += txt[i]
+        elif not txt[i].isnumeric() and txt[i] == '.':
+            resNumer += txt[i]
+        else:
+            if resNumer != '':
+                resTxt.append(resNumer)
+                resNumer = ''
+            else:
+                continue
+    resTxt.append(resNumer)
+
+    for i in symbols2:
+        resTxt[:] = [x for x in resTxt if x != i]
+    resTxt[:] = [x for x in resTxt if x != '']
+
+    # delete the symbols in the start and in the end
+    suffix = ('!', '?', ',', '.', ':', ';', '+', '-', '*', '/')
+
+    for i in range(len(resTxt)):
+        if not resTxt[i].isalpha() and not resTxt[i].isnumeric():
+            if resTxt[i].startswith(suffix) or resTxt[i].endswith(suffix):
+                while True:
+                    if resTxt[i].startswith(suffix):
+                        resTxt[i] = resTxt[i][1:]
+                        continue
+                    elif resTxt[i].endswith(suffix):
+                        resTxt[i] = resTxt[i][:len(resTxt[i]) - 1]
+                        continue
+                    break
+
+    print()
+    for i in resTxt:
+        print(i)
+
+#======================main=============================#
+
+txt_in = "-12.3-35=40, 50*10+30/40 Hello, " \
          ":!.word2023!0.23/cup:?- How\nis\tgoing your deals?" \
          " Are you ready.for New Summer 2024."
 
-# divide phrases complexity on the words and numbers
-print(txt)
-symbols2 = "!?.,:;+-*/"
-resAlpha: str = ''
-resNumer: str = ''
-resTxt = []
-for i in range(len(txt)):
-    if txt[i].isalpha():
-        resAlpha += txt[i]
-    else:
-        if resAlpha != '':
-            resTxt.append(resAlpha)
-            resAlpha = ''
-        else:
-            continue
-resTxt.append(resAlpha)
-
-for i in range(len(txt)):
-    if txt[i].isnumeric():
-        resNumer += txt[i]
-    elif not txt[i].isnumeric() and txt[i] == '.':
-        resNumer += txt[i]
-    else:
-        if resNumer != '':
-            resTxt.append(resNumer)
-            resNumer = ''
-        else:
-            continue
-resTxt.append(resNumer)
-
-for i in symbols2:
-    resTxt[:] = [x for x in resTxt if x != i]
-resTxt[:] = [x for x in resTxt if x != '']
-
-# delete the symbols in the start and in the end
-suffix = ('!', '?', ',', '.', ':', ';', '+', '-', '*', '/')
-
-for i in range(len(resTxt)):
-    if not resTxt[i].isalpha() and not resTxt[i].isnumeric():
-        if resTxt[i].startswith(suffix) or resTxt[i].endswith(suffix):
-            while True:
-                if resTxt[i].startswith(suffix):
-                    resTxt[i] = resTxt[i][1:]
-                    continue
-                elif resTxt[i].endswith(suffix):
-                    resTxt[i] = resTxt[i][:len(resTxt[i]) - 1]
-                    continue
-                break
-
-print()
-for i in resTxt:
-    print(i)
-
-# txt2 = "!Word"
-# print(txt2[0].isupper())
-# print(txt2[1].isupper())
+# function in order to divide phrases complexity on the words and numbers
+arr_word_num(txt_in)
--- menu.py
+++ menu.py
@@ -7,10 +7,10 @@ def clean_s() -> None:
 
 def exit_menu():
     while True:
-        out_submenu = input("\texit -> ")
+        out_submenu = input("\texit ")
         if not out_submenu:
             os.system('CLS')
-            return True
+            return 1
         else:
             print("ERROR!")
             continue
@@ -51,55 +51,190 @@ def receive_pos(ind_pos=0, name_f = ""):
 
         return ind_pos, direct
 
+#=========FUNCTION TO SPLIT ON SEPARATELY WORDS & NUMBERS=========#
+
+def arr_word_num(txt):
+    # print(txt)
+    symbols2 = "!?.,:;+-*/"
+    resAlpha: str = ''
+    resNumer: str = ''
+    resTxt = []
+    for i in range(len(txt)):
+        if txt[i].isalpha():
+            resAlpha += txt[i]
+        else:
+            if resAlpha != '':
+                resTxt.append(resAlpha)
+                resAlpha = ''
+            else:
+                continue
+    resTxt.append(resAlpha)
+
+    for i in range(len(txt)):
+        if txt[i].isnumeric():
+            resNumer += txt[i]
+        elif not txt[i].isnumeric() and txt[i] == '.':
+            resNumer += txt[i]
+        else:
+            if resNumer != '':
+                resTxt.append(resNumer)
+                resNumer = ''
+            else:
+                continue
+    resTxt.append(resNumer)
+
+    for i in symbols2:
+        resTxt[:] = [x for x in resTxt if x != i]
+    resTxt[:] = [x for x in resTxt if x != '']
+
+    # delete the symbols in the start and in the end
+    suffix = ('!', '?', ',', '.', ':', ';', '+', '-', '*', '/')
+
+    for i in range(len(resTxt)):
+        if not resTxt[i].isalpha() and not resTxt[i].isnumeric():
+            if resTxt[i].startswith(suffix) or resTxt[i].endswith(suffix):
+                while True:
+                    if resTxt[i].startswith(suffix):
+                        resTxt[i] = resTxt[i][1:]
+                        continue
+                    elif resTxt[i].endswith(suffix):
+                        resTxt[i] = resTxt[i][:len(resTxt[i]) - 1]
+                        continue
+                    break
+
+    # print()
+    # for i in resTxt:
+    #     print(i)
+
+    return resTxt
+
 #===================MENU SECTION REPORT===========================#
 
-def func_1(ind_m, ind_s, menu_s, txt: str): #Num symbols
-    print("{}.{} {}".format(ind_m + 1, ind_s + 1, menu_s[ind_s][0]))
-    if exit_menu():
-        return 0
+def func_1(ind_m, ind_s, menu_s, txt: str, logic: bool = ''): #Num symbols
+    ind_s = 0 if logic else ind_s
+    print("\n{}.{} {}".format(ind_m + 1, ind_s + 1, menu_s[ind_s][0]))
 
-def func_2(ind_m, ind_s, menu_s, txt: str): #Num digits
-    print("{}.{} {}".format(ind_m + 1, ind_s + 1, menu_s[ind_s][0]))
-    if exit_menu():
-        return 0
+    num_sym = len(txt)
+    print("\tQty of symbols -> ", num_sym if num_sym else "NO TEXT!")
 
-def func_3(ind_m, ind_s, menu_s, txt: str): #Num numbers
-    print("{}.{} {}".format(ind_m + 1, ind_s + 1, menu_s[ind_s][0]))
-    if exit_menu():
-        return 0
+    return 0 if logic else exit_menu()
 
-def func_4(ind_m, ind_s, menu_s, txt: str): #Num letters
-    print("{}.{} {}".format(ind_m + 1, ind_s + 1, menu_s[ind_s][0]))
-    if exit_menu():
-        return 0
+def func_2(ind_m, ind_s, menu_s, txt: str, logic: bool = ''): #Num digits
+    ind_s = 1 if logic else ind_s
+    print("\n{}.{} {}".format(ind_m + 1, ind_s + 1, menu_s[ind_s][0]))
 
-def func_5(ind_m, ind_s, menu_s, txt: str): #Num Up letters
-    print("{}.{} {}".format(ind_m + 1, ind_s + 1, menu_s[ind_s][0]))
-    if exit_menu():
-        return 0
+    num_digit = 0
+    for i in txt:
+        if i.isdigit():
+            num_digit += 1
 
-def func_6(ind_m, ind_s, menu_s, txt: str): #Num Low letters
-    print("{}.{} {}".format(ind_m + 1, ind_s + 1, menu_s[ind_s][0]))
-    if exit_menu():
-        return 0
+    num_sym = len(txt)
+    print("\tQty of digits -> ", num_digit if num_sym else "NO TEXT!")
 
-def func_7(ind_m, ind_s, menu_s, txt: str): #Num spaces
-    print("{}.{} {}".format(ind_m + 1, ind_s + 1, menu_s[ind_s][0]))
-    if exit_menu():
-        return 0
+    return 0 if logic else exit_menu()
 
-def func_8(ind_m, ind_s, menu_s, txt: str): #Num words, numbers
-    print("{}.{} {}".format(ind_m + 1, ind_s + 1, menu_s[ind_s][0]))
-    if exit_menu():
-        return 0
+def func_3(ind_m, ind_s, menu_s, txt: str, logic: bool = ''): #Num letters
+    ind_s = 2 if logic else ind_s
+    print("\n{}.{} {}".format(ind_m + 1, ind_s + 1, menu_s[ind_s][0]))
+
+    num_letter = 0
+    for i in txt:
+        if i.isdigit():
+            num_letter += 1
+
+    num_sym = len(txt)
+    print("\tQty of letters -> ", num_letter if num_sym else "NO TEXT!")
+
+    return 0 if logic else exit_menu()
+
+def func_4(ind_m, ind_s, menu_s, txt: str, logic: bool = ''): #Num Up letters
+    ind_s = 3 if logic else ind_s
+    print("\n{}.{} {}".format(ind_m + 1, ind_s + 1, menu_s[ind_s][0]))
+
+    num_up_letter = 0
+    for i in txt:
+        if i.isupper():
+            num_up_letter += 1
+
+    num_sym = len(txt)
+    print("\tQty of Up letters -> ", num_up_letter if num_sym else "NO TEXT!")
+
+    return 0 if logic else exit_menu()
+
+def func_5(ind_m, ind_s, menu_s, txt: str, logic: bool = ''): #Num Low letters
+    ind_s = 4 if logic else ind_s
+    print("\n{}.{} {}".format(ind_m + 1, ind_s + 1, menu_s[ind_s][0]))
+
+    num_low_letter = 0
+    for i in txt:
+        if i.islower():
+            num_low_letter += 1
+
+    num_sym = len(txt)
+    print("\tQty of Low letters -> ", num_low_letter if num_sym else "NO TEXT!")
+
+    return 0 if logic else exit_menu()
+
+def func_6(ind_m, ind_s, menu_s, txt: str, logic: bool = ''): #Num spaces
+    ind_s = 5 if logic else ind_s
+    print("\n{}.{} {}".format(ind_m + 1, ind_s + 1, menu_s[ind_s][0]))
+
+    num_space = 0
+    for i in txt:
+        if i == ' ':
+            num_space += 1
+
+    num_sym = len(txt)
+    print("\tQty of spaces -> ", num_space if num_sym else "NO TEXT!")
+
+    return 0 if logic else exit_menu()
+
+def func_7(ind_m, ind_s, menu_s, txt_in: str, logic: bool = ''): #Num numbers
+    ind_s = 6 if logic else ind_s
+    print("\n{}.{} {}".format(ind_m + 1, ind_s + 1, menu_s[ind_s][0]))
+
+    arr_wn = arr_word_num(txt_in)
+    arr_num = []
+    num_numbers = 0
+
+    for i in arr_wn:
+        if not i.isalpha() and i.find('.') != -1:
+            arr_num.append(i)
+            num_numbers += 1
+        elif i.isnumeric():
+            arr_num.append(i)
+            num_numbers += 1
+
+    print("Numbers\t-> ", arr_num)
+    num_sym = len(txt_in)
+    print(" Qty of numbers -> ", num_numbers if num_sym else "NO TEXT!")
+
+    return 0 if logic else exit_menu()
+
+def func_8(ind_m, ind_s, menu_s, txt_in: str, logic: bool = ''): #Num words
+    ind_s = 7 if logic else ind_s
+    print("\n{}.{} {}".format(ind_m + 1, ind_s + 1, menu_s[ind_s][0]))
+    if not len(txt_in):
+        print("NO TEXT")
+        exit_menu()
+
+    arr_wn = arr_word_num(txt_in)
+    sentence = ' '.join(arr_wn)
+
+    for i in arr_wn:
+        if i.isalpha():
+            print(i, "\t= ", sentence.count(i), " p.c.")
+
+    return 0 if logic else exit_menu()
 
 def func_9(ind_m, ind_s, menu_s, txt: str): #Total report
-    print("{}.{} {}".format(ind_m + 1, ind_s + 1, menu_s[ind_s][0]))
-    if exit_menu():
-        return 0
+    print("\n{}.{} {}".format(ind_m + 1, ind_s + 1, menu_s[ind_s][0]))
+    for i in range(len(menu_s)-2):
+        menu_s[i][1](1, ind_s, menu_s, txt, True)
+    exit_menu()
 
 def func_10(ind_m, ind_s, menu_s, txt=None):
-    print("{}.{} {}".format(ind_m + 1, ind_s + 1, menu_s[ind_s][0]))
+    print("\n{}.{} {}".format(ind_m + 1, ind_s + 1, menu_s[ind_s][0]))
     return True
 
 #======================MENU FUNCTION=============================#
@@ -119,12 +254,12 @@ def report_txt(ind, txt):
     menu_sub = {
         0: ["Num symbols", func_1],
         1: ["Num digits", func_2],
-        2: ["Num numbers", func_3],
-        3: ["Num letters", func_4],
-        4: ["Num Up letters", func_5],
-        5: ["Num Low letters", func_6],
-        6: ["Num spaces", func_7],
-        7: ["Num words, numbers", func_8],
+        2: ["Num letters", func_3],
+        3: ["Num Up letters", func_4],
+        4: ["Num Low letters", func_5],
+        5: ["Num spaces", func_6],
+        6: ["Num numbers", func_7],
+        7: ["Num words", func_8],
         8: ["Total info", func_9],
         9: ["Exit", func_10]
     }
